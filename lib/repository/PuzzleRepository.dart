420a5904094d2800158f32ebf27f3204aac8db52
oriolbernal <obernalpo@gmail.com>
2021-06-29 23:35:35 +0200

    remove unused http dependency

--- lib/repository/PuzzleRepository.dart
+++ lib/repository/PuzzleRepository.dart
@@ -1,16 +1,14 @@
 import 'dart:async';
-import 'package:http/http.dart' as http;
-import 'dart:io';
 import 'dart:convert';
 import 'package:chessradio/model/puzzle.dart';
 import 'package:http/http.dart';
 
 class PuzzleRepository {
-  final String postsURL =
+  final String url =
       "https://raw.githubusercontent.com/chessradio/chessradio-db/main/puzzles.json";
 
   Future<List<Puzzle>> fetchPuzzles() async {
-    Response res = await get(Uri.parse(postsURL));
+    Response res = await get(Uri.parse(url));
     if (res.statusCode == 200) {
       List<dynamic> body = jsonDecode(res.body);
       List<Puzzle> puzzles = body
