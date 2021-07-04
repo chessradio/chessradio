d9be150fb039fc835bbbf23e1e4921f1bc15ff63
oriolbernal <obernalpo@gmail.com>
2021-07-04 20:49:19 +0200

    hovers

--- lib/ui/widgets/playlist/puzzle_widget.dart
+++ lib/ui/widgets/playlist/puzzle_widget.dart
@@ -22,23 +22,34 @@ class _PuzzleWidgetState extends State<PuzzleWidget> {
         onTap: widget.onTap,
         leading: Wrap(
           children: [
-            IconButton(
-                onPressed: null,
-                icon: widget.puzzle.playingAs == PieceColor.WHITE
-                    ? new Image.asset("assets/icons/white-piece.png")
-                    : new Image.asset("assets/icons/black-piece.png")),
-            IconButton(
-                onPressed: null,
-                icon: widget.puzzle.level == Level.EASY
-                    ? new Image.asset("assets/icons/easy.png")
-                    : widget.puzzle.level == Level.MEDIUM
-                        ? new Image.asset("assets/icons/medium.png")
-                        : new Image.asset("assets/icons/hard.png"))
+            Tooltip(
+                message: widget.puzzle.playingAs.toString().split('.').last +
+                    " pieces start",
+                child: IconButton(
+                    onPressed: null,
+                    icon: widget.puzzle.playingAs == PieceColor.WHITE
+                        ? new Image.asset("assets/icons/white-piece.png")
+                        : new Image.asset("assets/icons/black-piece.png"))),
+            Tooltip(
+                message:
+                    widget.puzzle.level.toString().split('.').last + " level",
+                child: IconButton(
+                    onPressed: null,
+                    icon: widget.puzzle.level == Level.EASY
+                        ? new Image.asset("assets/icons/easy.png")
+                        : widget.puzzle.level == Level.MEDIUM
+                            ? new Image.asset("assets/icons/medium.png")
+                            : new Image.asset("assets/icons/hard.png")))
           ],
         ),
-        trailing: IconButton(
-          icon: Icon(Icons.remove_red_eye, size: 30),
-          onPressed: () => print("show solution!"),
+        trailing: Tooltip(
+          message: "Show solution",
+          child: IconButton(
+            icon: Icon(Icons.remove_red_eye, size: 30),
+            onPressed: () async {
+              print('show solution!');
+            },
+          ),
         ),
       ),
     );
