22cc185d92d762641c6fc43a2d56115c273e99d2
Enric Moreu <enric.moreu.filella@gmail.com>
2021-08-07 15:35:08 +0200

    Border play arrow and remove shadow

--- lib/ui/widgets/playlist/control_buttons_widget.dart
+++ lib/ui/widgets/playlist/control_buttons_widget.dart
@@ -38,6 +38,10 @@ class ControlButtonsWidget extends StatelessWidget {
             } else if (playing != true) {
               return RawMaterialButton(
                 onPressed: player.play,
+                elevation: 0,
+                hoverElevation: 0,
+                focusElevation: 0,
+                highlightElevation: 0,
                 // elevation: 2.0,
                 fillColor: Colors.white,
                 child: Icon(
@@ -45,21 +49,34 @@ class ControlButtonsWidget extends StatelessWidget {
                   size: 40.0,
                 ),
                 // padding: EdgeInsets.all(15.0),
-                shape: CircleBorder(),
+                shape: CircleBorder(side: BorderSide(color: Colors.black)),
               );
             } else if (processingState != ProcessingState.completed) {
-              return IconButton(
-                icon: Icon(
+              return RawMaterialButton(
+                child: Icon(
                   Icons.pause,
                   color: Colors.black,
+                  size: 40.0,
                 ),
-                iconSize: 40.0,
                 onPressed: player.pause,
+                shape: CircleBorder(side: BorderSide(color: Colors.black)),
               );
             } else {
-              return IconButton(
-                icon: Icon(Icons.play_arrow),
-                iconSize: 64.0,
+              return RawMaterialButton(
+                // onPressed: player.play,
+                elevation: 0,
+                hoverElevation: 0,
+                focusElevation: 0,
+                highlightElevation: 0,
+                // elevation: 2.0,
+                fillColor: Colors.white,
+                child: Icon(
+                  Icons.play_arrow,
+                  size: 40.0,
+                ),
+                // padding: EdgeInsets.all(15.0),
+                shape: CircleBorder(side: BorderSide(color: Colors.black)),
+
                 onPressed: () => player.seek(Duration.zero,
                     index: player.effectiveIndices!.first),
               );
