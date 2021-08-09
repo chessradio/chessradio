7dfc6bf7f578c052e72bbc6c2de6e97ef5ef3346
Enric Moreu <enric.moreu.filella@gmail.com>
2021-08-10 01:31:11 +0200

    Get the app screen only in browser

---
+++ lib/ui/screens/gettheapp_screen.dart
@@ -0,0 +1,94 @@
+import 'package:chessradio/bloc/puzzles_bloc.dart';
+import 'package:chessradio/ui/screens/playlist_screen.dart';
+import 'package:chessradio/ui/widgets/bar/chess_radio_drawer_widget.dart';
+import 'package:chessradio/ui/widgets/bar/chess_radio_title_widget.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class GetTheAppScreen extends StatefulWidget {
+  @override
+  _GetTheAppScreenState createState() => _GetTheAppScreenState();
+}
+
+class _GetTheAppScreenState extends State<GetTheAppScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Get the app',
+      home: Scaffold(
+        appBar: AppBar(
+          title: ChessRadioTitleWidget('Get the app', false),
+          backgroundColor: Colors.black,
+          leading: IconButton(
+            icon: Icon(Icons.arrow_back),
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (context) => PlayListScreen(new PuzzlesBloc()),
+                ),
+              );
+            },
+          ),
+          actions: [
+            ChessRadioDrawerWidget(),
+          ],
+        ),
+        body: Column(
+          children: [
+            Container(
+              child: (Text(
+                '''Get the app and listen to Chess Radio on your smartphone.
+ ''',
+                style: GoogleFonts.montserrat(
+                  color: Colors.black,
+                  fontSize: 18,
+                  fontWeight: FontWeight.w300,
+                ),
+              )),
+              margin: const EdgeInsets.all(50),
+            ),
+            GestureDetector(
+              onTap: _launchURLAndroid,
+              child: Image.asset(
+                'assets/images/playstore.png',
+                height: 50,
+                fit: BoxFit.cover,
+              ),
+            ),
+            SizedBox(height: 10),
+            GestureDetector(
+              onTap: _launchURLiOS,
+              child: Image.asset(
+                'assets/images/appstore.png',
+                height: 50,
+                fit: BoxFit.cover,
+              ),
+            ),
+          ],
+        ),
+        backgroundColor: Colors.white,
+      ),
+    );
+  }
+}
+
+class SelectorScreen {}
+
+_launchURLAndroid() async {
+  const url = 'https://play.google.com/store/search?q=chessradio';
+  if (await canLaunch(url)) {
+    await launch(url);
+  } else {
+    throw 'Could not launch $url';
+  }
+}
+
+_launchURLiOS() async {
+  const url = 'https://www.apple.com/us-hed/search/chessradio';
+  if (await canLaunch(url)) {
+    await launch(url);
+  } else {
+    throw 'Could not launch $url';
+  }
+}
--- lib/ui/widgets/bar/chess_radio_drawer_widget.dart
+++ lib/ui/widgets/bar/chess_radio_drawer_widget.dart
@@ -1,7 +1,9 @@
 import 'package:chessradio/ui/screens/about_screen.dart';
 import 'package:chessradio/ui/screens/feedback_screen.dart';
+import 'package:chessradio/ui/screens/gettheapp_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter/foundation.dart' show kIsWeb;
 
 class ChessRadioDrawerWidget extends StatefulWidget {
   @override
@@ -14,6 +16,32 @@ class _ChessRadioDrawerWidgetState extends State<ChessRadioDrawerWidget> {
     return Container(
       child: PopupMenuButton<ElevatedButton>(itemBuilder: (context) {
         return [
+          if (kIsWeb)
+            PopupMenuItem(
+              child: ElevatedButton(
+                child: Center(
+                  child: Text(
+                    'Get the app',
+                    style: GoogleFonts.montserrat(
+                        color: Colors.white,
+                        fontSize: 16,
+                        fontWeight: FontWeight.w300),
+                  ),
+                ),
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all<Color>(
+                    Colors.black,
+                  ),
+                ),
+                onPressed: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (context) => GetTheAppScreen(),
+                    ),
+                  );
+                },
+              ),
+            ),
           PopupMenuItem(
             child: ElevatedButton(
               child: Center(
