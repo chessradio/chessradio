5c038b46685e37a7a24ba664da61f3c92ff7f5b4
Enric Moreu <enric.moreu.filella@gmail.com>
2021-08-13 19:43:11 +0200

    Build web V2 with new favicon

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -3,28 +3,28 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "assets/AssetManifest.json": "d6850d0b2ac753f642bd9f30dbe545de",
-"assets/assets/icons/black-piece.png": "169df01d09e6359bbe2b5865b0329b13",
-"assets/assets/icons/easy.png": "7d6db27cddab859412d859535d51b4f1",
-"assets/assets/icons/hard.png": "68591dd893f0a37333355e05f94d3ac5",
-"assets/assets/icons/medium.png": "7db6c30fffd666c5c602b1f42e0e3d83",
-"assets/assets/icons/white-piece.png": "7c2d708d9aa622c4aa6cc69e68ffa1b4",
-"assets/assets/images/appstore.png": "d45b7b445ec6aa479be9dd9f2b627c94",
-"assets/assets/images/demo.png": "28ff37e436f06ec97f22fd4c9853a3c3",
-"assets/assets/images/floral.png": "c941a59010d232b6c8bfa009a4c8b505",
-"assets/assets/images/playstore.png": "3ad61751a6456c3a72ff24590c920dd3",
-"assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
-"assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1",
-"assets/NOTICES": "e120bfb5a9d62f61f39b1da08924e51e",
-"assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
-"favicon.png": "5dcef449791fa27946b3d35ad8803796",
+  "version.json": "ae8e8b33b690f239756c14a2052d020b",
+"favicon.ico": "fb463dc14d1814caa1e2a8d3fd940432",
+"index.html": "76d53f23d86ea217884078f17caec8fc",
+"/": "76d53f23d86ea217884078f17caec8fc",
+"main.dart.js": "74bed6695ec9d3aec7f0ef9539c6f649",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "04b849efe67e16656585b2cefbca420e",
-"/": "04b849efe67e16656585b2cefbca420e",
-"main.dart.js": "191980b08dac595f9885cab3894b48b3",
-"manifest.json": "d19e0aa3959103b21aab3b3b8834d225",
-"version.json": "ae8e8b33b690f239756c14a2052d020b"
+"manifest.json": "96de49731ca82fbacd2dc8e8c52d13d0",
+"assets/AssetManifest.json": "f4d4b5de925dff6996e4b91fc35867e5",
+"assets/NOTICES": "70dbc2c5e9a608dd2b76e6ab5f2c88ab",
+"assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
+"assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
+"assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1",
+"assets/assets/images/demo.png": "28ff37e436f06ec97f22fd4c9853a3c3",
+"assets/assets/images/appstore.png": "d45b7b445ec6aa479be9dd9f2b627c94",
+"assets/assets/images/floral.png": "c941a59010d232b6c8bfa009a4c8b505",
+"assets/assets/images/playstore.png": "3ad61751a6456c3a72ff24590c920dd3",
+"assets/assets/icons/white-piece.png": "7c2d708d9aa622c4aa6cc69e68ffa1b4",
+"assets/assets/icons/black-piece.png": "169df01d09e6359bbe2b5865b0329b13",
+"assets/assets/icons/medium.png": "7db6c30fffd666c5c602b1f42e0e3d83",
+"assets/assets/icons/easy.png": "7d6db27cddab859412d859535d51b4f1",
+"assets/assets/icons/hard.png": "68591dd893f0a37333355e05f94d3ac5"
 };
 
 // The application shell files that are downloaded before a service worker can
